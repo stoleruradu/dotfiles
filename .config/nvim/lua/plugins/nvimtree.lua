f2375b6795c2f285a74bf84251496f488e788315
Stoleru Radu <10254524+stoleruradu@users.noreply.github.com>
2024-02-19 20:57:04 +0200

    fix: alacritty tweaks

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -15,7 +15,8 @@ return {
       vim.keymap.set("n", "r", api.fs.rename, opts "Rename")
       vim.keymap.set("n", "R", api.tree.reload, opts "Refresh")
       vim.keymap.set("n", "I", api.tree.toggle_gitignore_filter, opts "Toggle Git Ignore")
-      vim.keymap.set('n', 'o', api.node.open.vertical, opts('Open: Vertical Split'));
+      vim.keymap.set("n", "o", api.node.open.edit, opts "Open")
+      vim.keymap.set('n', 's', api.node.open.vertical, opts('Open: Vertical Split'));
     end,
     view = {
       side = 'left',
