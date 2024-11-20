7b94a6febfe6cbe76afd770ba4a792cc61bb8f96
radu <10254524+stoleruradu@users.noreply.github.com>
2024-11-20 18:35:38 +0200

    feat: add docker to path

--- home.nix
+++ home.nix
@@ -79,6 +79,8 @@
       [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" # This loads nvm
       [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
+    export PATH=$PATH:~/.docker/bin
+
     alias tmux_init='tmux new -s $PWD:t:r'
   '';
 
