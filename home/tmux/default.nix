3eb6a391993a5255b261da322afa6a4dc73cba2f
Radu <10254524+stoleruradu@users.noreply.github.com>
2025-01-28 19:51:58 +0200

    feat: small tweaks (#11)
    * feat: add docker to path
    
    * fix: tmux stying
    
    * chore: lock update

--- home/tmux/default.nix
+++ home/tmux/default.nix
@@ -5,7 +5,26 @@
     enable = true;
     sensibleOnTop = false;
     plugins = [
-      pkgs.tmuxPlugins.catppuccin
+      { 
+        plugin = pkgs.tmuxPlugins.catppuccin;
+        extraConfig = ''
+            set -g @catppuccin_flavour 'mocha'
+            set -g @catppuccin_window_left_separator ""
+            set -g @catppuccin_window_right_separator " "
+            set -g @catppuccin_window_middle_separator " █"
+            set -g @catppuccin_window_number_position "right"
+            set -g @catppuccin_window_default_fill "number"
+            set -g @catppuccin_window_default_text "#W"
+            set -g @catppuccin_window_current_fill "number"
+            set -g @catppuccin_window_current_text "#W"
+            set -g @catppuccin_status_modules_right "application session user"
+            set -g @catppuccin_status_left_separator  " "
+            set -g @catppuccin_status_right_separator ""
+            set -g @catppuccin_status_right_separator_inverse "no"
+            set -g @catppuccin_status_fill "icon"
+            set -g @catppuccin_status_connect_separator "no"
+        '';
+      }
     ];
     extraConfig = builtins.readFile ./tmux.conf;
   };
